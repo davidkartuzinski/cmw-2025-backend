4922039387279fd7c81ffc8e3e9454a6d64b77a4
davidkartuzinski <david@kaidawei.me>
2025-09-20 19:05:37 +0200

    add Cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,43 @@
-module.exports = ({ env }) => ({
-  'populate-deep': {
-    config: {
-      defaultDepth: 5, // you can tweak this number
+// config/plugins.js
+module.exports = ({ env }) => {
+  // Choose provider via env. Defaults:
+  // - production => Cloudinary (if creds present)
+  // - other envs => local
+  const useCloudinary = env.bool(
+    'UPLOAD_USE_CLOUDINARY',
+    env('NODE_ENV') === 'production' &&
+      !!env('CLOUDINARY_NAME') &&
+      !!env('CLOUDINARY_KEY') &&
+      !!env('CLOUDINARY_SECRET')
+  );
+
+  return {
+    'populate-deep': {
+      config: { defaultDepth: 5 },
     },
-  },
-});
+
+    upload: {
+      config: useCloudinary
+        ? {
+            provider: '@strapi/provider-upload-cloudinary',
+            providerOptions: {
+              cloud_name: env('CLOUDINARY_NAME'),
+              api_key: env('CLOUDINARY_KEY'),
+              api_secret: env('CLOUDINARY_SECRET'),
+            },
+            actionOptions: {
+              upload: {},
+              delete: {},
+            },
+          }
+        : {
+            // Local provider for dev
+            provider: 'local',
+            providerOptions: {
+              // Optional: customize local folder (defaults to public/uploads)
+              // sizeLimit: 10 * 1024 * 1024, // 10MB
+            },
+          },
+    },
+  };
+};
